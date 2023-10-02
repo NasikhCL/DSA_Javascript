978241e43d553329ee23b45e6dbb7595130e60bb
Nasikh CL <nasikcl@gmail.com>
2023-10-02 14:25:59 +0530

    prime number problem

---
+++ prime.js
@@ -0,0 +1,46 @@
+//given a natural number 'n', determine if the number is prime or not
+
+
+//soln
+
+function isPrime(n){
+    if(n<2){
+        return false;
+    }
+    for(let i=2; i<n; i++){
+        if(n%i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime(1))
+console.log(isPrime(5))
+console.log(isPrime(4))
+
+// Big-O = O(n)
+
+
+
+// more optimized soln
+
+function isPrimeOptimized(n){
+    if(n<2){
+        return false;
+    }
+    for(let i=2; i<= Math.sqrt(n); i++){
+        if(n%i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrimeOptimized(1))
+console.log(isPrimeOptimized(5))
+console.log(isPrimeOptimized(4))
+
+
+// Big-O = O(sqrt(n))
+
